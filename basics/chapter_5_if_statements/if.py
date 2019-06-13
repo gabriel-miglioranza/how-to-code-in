893a48aac4217350bc3b44ce9a0b3fbe61d6eca9
gabriel-miglioranza <gabrimig@gmail.com>
2019-06-13 13:32:41 +0000

    End of chapter 6

--- basics/chapter_5_if_statements/if.py
+++ basics/chapter_5_if_statements/if.py
@@ -154,4 +154,51 @@ elif age >= 18 and age <= 65:
 
 # Testing Multiple Conditions
 # The if-elif-else chain permits only one test to pass, if you want to check all the conditions of interest you need test one by one separately.
+requested_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print('Adding mushrooms.')
+if 'pepperoni' in requested_toppings:
+    print('Adding pepperoni.')
+if 'extra cheese' in requested_toppings:
+    print('Adding extra cheese.')
+
+print('\nFinished making your pizza!')
+
+# Using if Statements with Lists
+
+# Checking for Special Items
+# Put a if statement inside a for loop to check for a special item in a list.
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print('Sorry, we are out of green peppers right now.')
+    else: 
+        print('Adding' + requested_topping + '.')
+
+print('\nFinished making your pizza!')
+
+# Checking That a List Is Not Empty
+requested_toppings = []
+# The following line checks if requested_toppings is empty.
+if requested_toppings: 
+    for requested_topping in requested_toppings: 
+        print('Adding' + requested_topping + '.')
+    print('\nFinished making your pizza!')
+else:
+    print('Are you sure you want a plain pizza?')
+
+# Using Multiple Lists
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    # The next line checks if the item (requested_topping) from the list
+    # (requested_toppings) is in the list (available_toppings).
+    if requested_topping in available_toppings:
+        print('Adding'+ requested_topping + '.')
+    else: 
+        print("Sorry, we don't have " + requested_topping + ".")
+
+print('\nFinished making your pizza!')
 
