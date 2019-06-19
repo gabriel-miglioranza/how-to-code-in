1d35fa66774f20bdbbd45519b4d4318852a33c2b
gabriel-miglioranza <gabrimig@gmail.com>
2019-06-19 13:01:26 +0000

    More on functions.

--- basics/chapter_8_functions/exercises_8.py
+++ basics/chapter_8_functions/exercises_8.py
@@ -14,3 +14,66 @@ def favorite_book(title):
 
 favorite_book('against method')
 
+# 8-3. T-Shirt
+def make_shirt(size, message):
+    '''Make a shirt with the desired size and message'''
+    print('Shirt size:' + size)
+    print('Message in the shirt: ' + message)
+
+make_shirt('M', 'The Beatles')
+make_shirt(size='M', message='Radiohead')
+
+# 8-4. Large Shirts
+def make_shirt(size='XG', message='I love Python!'):
+    '''Make a shirt with the desired size and message'''
+    print('Shirt size:' + size)
+    print('Message in the shirt: ' + message)
+
+make_shirt()
+make_shirt('M')
+make_shirt('P', 'Radiohead')
+
+# 8-5. Cities
+def describe_city(city, country='brazil'):
+    '''Display a simple information about a city'''
+    print(city.title() + ' is in ' + country.title() + '.')
+
+describe_city('são paulo')
+describe_city('london', 'england')
+describe_city(city='brasília')
+
+# 8-6. City Names
+def city_country(city, country):
+    return city.title() + ', ' + country.title()
+
+print(city_country('manaus','brazil')) 
+
+# 8-7. Album
+def make_album(artist, title, n_tracks=0):
+    album = {'artist': artist, 'title': title}
+    if n_tracks != 0:
+        album['number of tracks'] = n_tracks
+    return album
+
+ok_computer = make_album('radiohead', 'ok computer', 12)
+sgt_peppers = make_album('the beatles', "sgt. pepper's lonely hearts club band")
+print(ok_computer)
+print(sgt_peppers)
+
+# 8-8. User Albums
+while True:
+    print("\nPlease describe a music album:")
+    print("(enter 'q' at any time to quit)")
+    t_name = input('Title: ')
+    if t_name == 'q':
+        break
+    a_name = input('Artist: ')
+    if a_name == 'q':
+        break
+    number = input('Number of tracks (default = 0): ')
+    if number == 'q':
+        break
+    album = make_album(a_name, t_name, int(number))
+    print(album)
+print('Goodbye!')
+
--- basics/chapter_8_functions/functions.py
+++ basics/chapter_8_functions/functions.py
@@ -53,4 +53,75 @@ def describe_pet(pet_name, animal_type='dog'):
     print('My ' + animal_type + "'s name is " + pet_name.title() + '.')
 
 describe_pet('montanha')
-describe_pet('chaira', 'cat')
+describe_pet('chiara', 'cat')
+
+# Equivalent Function Calls
+# The following function calls have the same result.
+# A dog named Willie
+describe_pet('willie')
+describe_pet(pet_name='willie')
+
+# A hamster named Harry.
+describe_pet('harry', 'hamster')
+describe_pet(pet_name='harry', animal_type='hamster')
+describe_pet(animal_type='hamster', pet_name='harry')
+
+# Avoiding Argument Errors
+# If you forget to put an argument in the function call Python will return an error message.
+#describe_pet()
+
+# Return Values
+# A function can return a value to be assigned to a variable.
+
+# Returning a Simple Value
+def get_formatted_name(first_name, last_name):
+    '''Return a full name, neatly formatted.'''
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
+
+# Making an Argument Opition
+# In the last example a person can have a middle name or it doesn't. 
+# You can make a optional argument to circumvent this problem.
+def get_formatted_name(first_name, last_name, middle_name=''):
+    '''Return a full name, neatly formatted.'''
+    if middle_name:
+        full_name = first_name + ' ' + middle_name + ' ' + last_name
+    else:
+        full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
+
+musician = get_formatted_name('john', 'hooker', 'lee')
+print(musician)
+
+# Returning a Dictionary
+# A function can return any kind of value you need it to.
+
+def build_person(first_name, last_name):
+    '''Return a dictionary of information about a person.'''
+    person = {'first': first_name, 'last': last_name}
+    return person
+
+musician = build_person('jimi', 'hendrix')
+print(musician)
+
+# Using a Function with a while Loop
+
+while True:
+    print('\nPlease tell me your name:')
+    print("(enter 'q' at any time to quit)")
+    f_name = input('First name: ')
+    if f_name == 'q':
+        break
+    l_name = input('Last name: ')
+    if l_name == 'q':
+        break
+    formatted_name = get_formatted_name(f_name, l_name)
+    print('\nHello, ' + formatted_name + '!')
+print('Goodbye!')
+
